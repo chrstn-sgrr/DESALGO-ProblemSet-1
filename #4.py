adac3cd273e4cb593e69e6afbfa8d2d9fdb56c19
chrstn-sgrr <131838456+chrstn-sgrr@users.noreply.github.com>
2024-09-01 21:50:51 +0800

    Replaced arr with list

--- #4.py
+++ #4.py
@@ -6,17 +6,17 @@ a.) The algorithm described is known as selection sort.
 b.) Use a loop invariant to check that the algorithm is correct, and make sure that the loop invariant fulfills the 3 necessary properties.
 '''
 
-def selection_sort(arr):
-    # Traverse through all array elements
-    for i in range(len(arr)):
-        # Find the minimum element in remaining unsorted array
+def selection_sort(list):
+    # Traverse through all list elements
+    for i in range(len(list)):
+        # Find the minimum element in remaining unsorted list
         min_idx = i
-        for j in range(i+1, len(arr)):
-            if arr[j] < arr[min_idx]:
+        for j in range(i+1, len(list)):
+            if list[j] < list[min_idx]:
                 min_idx = j
         
         # Swap the found minimum element with the first element
-        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+        list[i], list[min_idx] = list[min_idx], list[i]
 
 # Example usage
 numbers = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
