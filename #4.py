49a485790926a79697b085deaf0761da743c55ce
chrstn-sgrr <131838456+chrstn-sgrr@users.noreply.github.com>
2024-09-01 15:48:21 +0800

    Added comment

--- #4.py
+++ #4.py
@@ -1,1 +1,7 @@
-# for question 4
+'''
+Consider sorting n numbers stored in array A by first finding the smallest element of A and exchanging it with the element A[0].
+Then find the second smallest element of A and exchange it with A[1]. Continue in this manner for the first n = 1 elements of A.
+
+a.) The algorithm described is known as selection sort. 
+b.) Use a loop invariant to check that the algorithm is correct, and make sure that the loop invariant fulfills the 3 necessary properties.
+'''
